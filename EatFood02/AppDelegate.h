e8086e673481f41a11b6ba476ad1012d3b938dd2
郝宇泽 <haoyuze@haoyuzedeMacBook-Pro.local>
2016-11-24 20:11:09 +0800

    Initial Commit

---
+++ EatFood02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EatFood02
+//
+//  Created by 郝宇泽 on 2016/11/24.
+//  Copyright © 2016年 twopoorcick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EatFood02/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EatFood02
+//
+//  Created by 郝宇泽 on 2016/11/24.
+//  Copyright © 2016年 twopoorcick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
