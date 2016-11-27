f7cfd626f360027ba7b3821be49d230d2d0eacfa
郝宇泽 <haoyuze@haoyuzedeMacBook-Pro.local>
2016-11-27 18:24:22 +0800

    eatfood selectfood animotion finish

--- EatFoodVC.m
+++ EatFoodVC.m
@@ -8,8 +8,10 @@
 
 #import "EatFoodVC.h"
 #import "PersonMessageVC.h"
+#import "JTNumberScrollAnimatedView.h"
 
 @interface EatFoodVC ()
+@property (weak, nonatomic) IBOutlet JTNumberScrollAnimatedView *animotionView;
 
 @end
 
@@ -21,78 +23,17 @@
     UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"person"] style:UIBarButtonItemStyleDone target:self action:@selector(personMessage)];
 
     self.navigationItem.rightBarButtonItem = rightButton;
+    
+    self.animotionView.textColor = [UIColor whiteColor];
+    self.animotionView.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:42];
+    
+    self.animotionView.minLength = 3;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
-}
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (void)personMessage{
     UIStoryboard *secondStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
@@ -101,6 +42,10 @@
     self.hidesBottomBarWhenPushed=YES;
     [self.navigationController pushViewController:pm animated:YES];
     self.hidesBottomBarWhenPushed=NO;
+}
+- (IBAction)select_cai:(id)sender {
+    [self.animotionView setValue:@"鱼香肉丝"];
+    [self.animotionView startAnimation];
 }
 
 @end
