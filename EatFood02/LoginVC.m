13abb51838f4a6fef9e4b86568247599a2f0c6ee
SmallWirldwind <390812516@qq.com>
2016-11-29 16:12:23 +0800

    login and register first commit

---
+++ EatFood02/LoginVC.m
@@ -0,0 +1,46 @@
+//
+//  LoginVC.m
+//  EatFood02
+//
+//  Created by 郝宇泽 on 2016/11/29.
+//  Copyright © 2016年 twopoorcick. All rights reserved.
+//
+
+#import "LoginVC.h"
+#import "ViewController.h"
+
+@interface LoginVC ()
+
+@end
+
+@implementation LoginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)Login:(id)sender {
+    UIStoryboard *MainStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    ViewController* viewcontroller = [MainStoryBoard instantiateViewControllerWithIdentifier:@"viewcontroller"];  //personmessagevc为viewcontroller的StoryboardId
+    
+//    [self.navigationController pushViewController:viewcontroller animated:YES];
+    [self.navigationController presentViewController:viewcontroller animated:YES completion:nil];
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ EatFood02/RegisterVC.h
@@ -0,0 +1,13 @@
+//
+//  RegisterVC.h
+//  EatFood02
+//
+//  Created by 郝宇泽 on 2016/11/29.
+//  Copyright © 2016年 twopoorcick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterVC : UIViewController
+
+@end
