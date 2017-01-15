3904b90876f99e16fc6a03483ba427a4b211e6f1
SmallWirldwind <390812516@qq.com>
2017-01-15 22:01:53 +0800

    注册流程开始更改

---
+++ EatFood02/HealthyVC.h
@@ -0,0 +1,13 @@
+//
+//  HealthyVC.h
+//  EatFood02
+//
+//  Created by 郝宇泽 on 2017/1/12.
+//  Copyright © 2017年 twopoorcick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HealthyVC : UITableViewController
+
+@end
---
+++ EatFood02/HealthyVC.m
@@ -0,0 +1,28 @@
+//
+//  HealthyVC.m
+//  EatFood02
+//
+//  Created by 郝宇泽 on 2017/1/12.
+//  Copyright © 2017年 twopoorcick. All rights reserved.
+//
+
+#import "HealthyVC.h"
+
+@interface HealthyVC ()
+
+@end
+
+@implementation HealthyVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
